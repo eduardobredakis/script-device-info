07e02d2db36bfbf0f4509191a157c86ee4ad4052
Eduardo Breda <eduardo.breda@kissolutions.tech>
2022-09-14 10:41:27 -0300

    fix issue where i cant find OS version

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from platform import java_ver, platform
+import platform
 import psutil
 import subprocess
 from uuid import getnode as get_mac
 
 #Operating System Version
-current_machine_OS = platform.system()
-current_machine_OS_version = platform.release()
-print(f"Your Operating System are: {current_machine_OS}, {current_machine_OS_version}")
+os_name = platform.system()
+os_version = platform.release()
+print(f"Your Operating System is: {os_name} {os_version}")
 
 #Device SerialNumber
 current_machine_id = subprocess.check_output('wmic bios get serialnumber').decode("utf-8") 
@@ -17,5 +17,9 @@ mac=get_mac()
 print(f"Your device Addres are: {(hex(mac))}")
 
 #Device amount of RAM installed and Used
+memory_amount = round(psutil.virtual_memory().total/1000000000, 2)
+memory_amount_used = round(psutil.virtual_memory().used/1000000000, 2)
 print(f"Total RAM installed: {round(psutil.virtual_memory().total/1000000000, 2)}")
 print(f"Total Used Ram: {round(psutil.virtual_memory().used/1000000000, 2)}")
+
+

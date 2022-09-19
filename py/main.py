55bbee168bb25544abdbb45c5842e67707d52a0f
Eduardo Breda <eduardo.breda@kissolutions.tech>
2022-09-19 10:39:12 -0300

    check email and import csv

--- py/main.py
+++ py/main.py
@@ -1,12 +1,16 @@
 
+import email
 import platform
 import psutil
 import subprocess
 from uuid import getnode as get_mac
 from getmac import get_mac_address as gma
 import os
+import csv
+
 
 def email_input():
+    #Check kiss email
     email = input("Enter your KIS email: ")
     print("You entered: ", email)
     check = input("Please, confirm your email: ")
@@ -19,29 +23,36 @@ def email_input():
         print("You entered: ", email)
         check = input("Please, confirm your email: ")
         
+    #Get device people name
+    os_get_username = os.environ.get('USERNAME')
+    print(f"Your username: {os_get_username}")
+
+    #Operating System Version
+    os_name = platform.system()
+    os_version = platform.release()
+    os_detailed_info = platform.platform()
+    print(f"Your Operating System are: {os_name} {os_version}")
+    print(f"The detailed version of your {os_name} {os_version} are {os_detailed_info}")
+
+    #Device SerialNumber
+    current_machine_id = subprocess.check_output('wmic bios get serialnumber').decode("utf-8") 
+    print(f"Your device {current_machine_id}")
+
+    #IP Addres
+    mac = get_mac()
+    print(f"Your device Addres are: {(hex(mac))}")
+
+    #Device amount of RAM installed and Used
+    memory_amount = round(psutil.virtual_memory().total/1000000000, 2)
+    memory_amount_used = round(psutil.virtual_memory().used/1000000000, 2)
+    print(f"Total RAM installed: {memory_amount}")
+    print(f"Total Used Ram: {memory_amount_used}")
+
+    #Create CSV File Data
+    with open('aCsv.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([email, os_get_username, os_name,current_machine_id]) 
+        
 email_input()
 
-#Get device people name
-os_get_username = os.environ.get('USERNAME')
-print(f"Your username: {os_get_username}")
 
-#Operating System Version
-os_name = platform.system()
-os_version = platform.release()
-os_detailed_info = platform.platform()
-print(f"Your Operating System are: {os_name} {os_version}")
-print(f"The detailed version of your {os_name} {os_version} are {os_detailed_info}")
-
-#Device SerialNumber
-current_machine_id = subprocess.check_output('wmic bios get serialnumber').decode("utf-8") 
-print(f"Your device {current_machine_id}")
-
-#IP Addres
-mac = get_mac()
-print(f"Your device Addres are: {(hex(mac))}")
-
-#Device amount of RAM installed and Used
-memory_amount = round(psutil.virtual_memory().total/1000000000, 2)
-memory_amount_used = round(psutil.virtual_memory().used/1000000000, 2)
-print(f"Total RAM installed: {memory_amount}")
-print(f"Total Used Ram: {memory_amount_used}")
